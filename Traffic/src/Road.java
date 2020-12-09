b57945649bfc0e39cdeb8ed2d55ed4b78f72d351
Marek Brož <theshadowcamel@gmail.com>
2020-12-09 20:11:04 +0100

    update

--- Traffic/src/Road.java
+++ Traffic/src/Road.java
@@ -1,10 +1,23 @@
 import javax.swing.*;
-import java.awt.Graphics;
+import java.awt.*;
 
 public class Road extends JPanel {
 
+    final int LANE_HEIGHT = 150;
+
+    public Road(){
+        super();
+    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,this.getWidth(), this.getHeight());
+        g.setColor(Color.WHITE);
+        for (int i = LANE_HEIGHT; i < 600; i = i + LANE_HEIGHT) {
+            for (int j = 0; j < getWidth() ; j = j + 40) {
+                g.fillRect(j, i, 30,5);
+            }
+        }
     }
 }
--- Traffic/src/Traffic.java
+++ Traffic/src/Traffic.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
+
+//TODO watch the videos! https://www.youtube.com/watch?v=NJFvpozCasA
 
 public class Traffic {
     JFrame frame = new JFrame("Traffic Simulation");
+    Road road = new Road();
 
     public Traffic (){
         frame.setSize(800,  600);
+        frame.setLayout(new BorderLayout());
+        frame.add(road, BorderLayout.CENTER);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
